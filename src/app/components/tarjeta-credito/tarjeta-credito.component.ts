b99027f116e01a9d27c0f81221508f8423c4c672
Ricardo <ricarox3@gmail.com>
2021-03-09 09:28:58 -0400

    Ejemplo-listado

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjeta-credito.component.css']
 })
 export class TarjetaCreditoComponent implements OnInit {
+  listTarjetas: any[]=[
+    {titular: 'Juan Perez', numerotarjeta: '154540', fechaExpiracion: '11/12', cvv:'123'},
+    {titular: 'Ricardo', numerotarjeta: '1565644356', fechaExpiracion: '05/09', cvv:'050'}
+  ];
 
   constructor() { }
 
